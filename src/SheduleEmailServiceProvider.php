58d008810a0dd79342aad57dd46d8b695cec5496
agoalofalife <agoalofalife@gmail.com>
2017-09-13 15:36:18 +0300

    init

---
+++ src/SheduleEmailServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Laravel\Passport;
+
+use Illuminate\Support\ServiceProvider;
+
+class SheduleEmailServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+}
