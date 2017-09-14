fe903c4fe7a7b10ab8d96f821635b2bf3ec78e1c
agoalofalife <agoalofalife@gmail.com>
2017-09-14 09:46:31 +0300

    add column in table

--- src/Parser.php
+++ src/Parser.php
@@ -10,6 +10,10 @@ class Parser
     public function parse()
     {
         $now = Carbon::now();
+
+
+
+        SheduleEmail::whereRaw('CAST(date AS Datetime) > ?', [$now])->get();
         SheduleEmail::all()->each(function ($value, $key) use ($now) {
             if ($value->date > $now) {
                 $mode = FactoryMode::get($value->mode);
