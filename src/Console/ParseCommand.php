0a45f5303ecf4b5f8d17dd3cebf98bfa8e1ba814
agoalofalife <agoalofalife@gmail.com>
2017-09-14 11:34:27 +0300

    add command parse

---
+++ src/Console/ParseCommand.php
@@ -0,0 +1,32 @@
+<?php
+namespace agoalofalife\postman\Console;
+
+use agoalofalife\postman\Parser;
+use Illuminate\Console\Command;
+
+class ParseCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'postman:parse';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Run the commands parse database and sends email';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Parser::parse();
+    }
+}
