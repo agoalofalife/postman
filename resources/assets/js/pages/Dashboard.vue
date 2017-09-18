e204dd6fe131434070c3a02377d98489b1d495a0
agoalofalife <agoalofalife@gmail.com>
2017-09-18 12:12:56 +0300

    add table in component dashboard

--- resources/assets/js/pages/Dashboard.vue
+++ resources/assets/js/pages/Dashboard.vue
@@ -1,72 +1,85 @@
 <template>
-    <!--<el-row>-->
-        <!--<el-col :span="20">-->
-    <div>
-            <p>Hello</p>
-            <p>Hello</p>
-            <p>Hello</p>
-    </div>
-    <!--<el-table-->
-            <!--:data="tableData"-->
-            <!--border-->
-            <!--style="width: 100%">-->
-        <!--<el-table-column-->
-                <!--fixed-->
-                <!--prop="date"-->
-                <!--label="Date"-->
-                <!--width="150">-->
-        <!--</el-table-column>-->
-        <!--<el-table-column-->
-                <!--prop="name"-->
-                <!--label="Name"-->
-                <!--width="120">-->
-        <!--</el-table-column>-->
-        <!--<el-table-column-->
-                <!--prop="state"-->
-                <!--label="State"-->
-                <!--width="120">-->
-        <!--</el-table-column>-->
-        <!--<el-table-column-->
-                <!--prop="city"-->
-                <!--label="City"-->
-                <!--width="120">-->
-        <!--</el-table-column>-->
-        <!--<el-table-column-->
-                <!--prop="address"-->
-                <!--label="Address"-->
-                <!--width="300">-->
-        <!--</el-table-column>-->
-        <!--<el-table-column-->
-                <!--prop="zip"-->
-                <!--label="Zip"-->
-                <!--width="120">-->
-        <!--</el-table-column>-->
-        <!--<el-table-column-->
-                <!--fixed="right"-->
-                <!--label="Operations"-->
-                <!--width="120">-->
-            <!--<template scope="scope">-->
-                <!--<el-button @click="handleClick" type="text" size="small">Detail</el-button>-->
-                <!--<el-button type="text" size="small">Edit</el-button>-->
-            <!--</template>-->
-        <!--</el-table-column>-->
-    <!--</el-table>-->
-        <!--</el-col>-->
-        <!--</el-row>-->
+    <el-row class="table-postman">
+        <el-col :span="16" :offset="4">
+    <el-table
+            :data="tableData"
+            border
+            style="width: 100%">
+        <el-table-column
+                fixed
+                prop="number"
+                label="№"
+                width="40">
+        </el-table-column>
+        <el-table-column
+                fixed
+                prop="dateStart"
+                label="Date start"
+                width="170">
+        </el-table-column>
+        <el-table-column
+                prop="theme"
+                label="Theme"
+                width="180">
+        </el-table-column>
+        <el-table-column
+                prop="text"
+                label="Text"
+                width="400">
+        </el-table-column>
+        <el-table-column
+                prop="mode"
+                label="Mode"
+                width="130">
+        </el-table-column>
+        <el-table-column
+                prop="status"
+                label="Status"
+                width="120">
+        </el-table-column>
+        <el-table-column
+                prop="updateAt"
+                label="Update At"
+                width="120">
+        </el-table-column>
+        <el-table-column
+                fixed="right"
+                label="Operations"
+                width="120">
+            <template scope="scope">
+                <el-button @click="handleClick" type="text" size="small">Detail</el-button>
+                <el-button type="text" size="small">Edit</el-button>
+            </template>
+        </el-table-column>
+    </el-table>
+        </el-col>
+        </el-row>
 </template>
 
 <style>
-    body{
-        background: red;
+    .table-postman{
+        margin-top: 7%;
     }
 </style>
 <script>
-
-    export default{
-        data(){
-            return{
-                msg:'hello vue'
+    export default {
+        methods: {
+            handleClick() {
+                console.log('click');
             }
         },
+        data() {
+            return {
+                tableData: [{
+                    number: '1',
+                    dateStart: '2016-05-03 12:42',
+                    theme: 'California',
+                    text: 'Los Angeles',
+                    mode: 'Every',
+                    status: 'Success',
+                    updateAt: '2016-05-03'
+                }]
+            }
+        }
     }
 </script>
