a1ab080cedaca9f6ad1eddbc8afa6224f7611ed9
agoalofalife <agoalofalife@gmail.com>
2017-10-08 19:37:53 +0300

    set test envirenment

---
+++ tests/Unit/Modes/OneToAllTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace agoalofalife\Tests\Modes;
+
+use Tests\TestCase;
+
+class OneToAllTest extends TestCase
+{
+    public function test_test()
+    {
+        $this->assertFalse(false);
+    }
+}
