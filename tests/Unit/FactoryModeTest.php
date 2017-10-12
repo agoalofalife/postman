cd017053b498330f917879db592dd1d04a0e20f7
agoalofalife <agoalofalife@gmail.com>
2017-10-12 12:34:37 +0300

    add refactoring test FactoryModeTest

--- tests/Unit/FactoryModeTest.php
+++ tests/Unit/FactoryModeTest.php
@@ -6,29 +6,24 @@ use agoalofalife\postman\FactoryMode;
 use agoalofalife\postman\Modes\Each;
 use agoalofalife\postman\Modes\OneToAll;
 
-/**
- * @runTestsInSeparateProcesses
- * @preserveGlobalState disabled
- * This up separate process for alias mock
- * More @link https://laracasts.com/discuss/channels/testing/mocking-a-class-persists-over-tests
- */
 class FactoryModeTest extends TestCase
 {
     public function testGetOneToAll() : void
     {
-        $mock = $this->mock('alias:agoalofalife\postman\FactoryMode');
-        $mockMode = $this->mock(OneToAll::class);
-        $mock->shouldReceive('get')->andReturn($mockMode);
-
-        FactoryMode::get(1);
+        $this->assertInstanceOf(OneToAll::class, FactoryMode::get(1));
     }
 
     public function testGetOneToEach() : void
     {
-        $mock = $this->mock('alias:agoalofalife\postman\FactoryMode');
-        $mockMode = $this->mock(Each::class);
-        $mock->shouldReceive('get')->andReturn($mockMode);
+        $this->assertInstanceOf(Each::class, FactoryMode::get(2));
+    }
 
-        FactoryMode::get(2);
+    /**
+     * @expectedException    \Exception
+     * @expectedExceptionMessage Mode not defined
+     */
+    public function testExpectedException() : void
+    {
+        FactoryMode::get(3);
     }
 }
