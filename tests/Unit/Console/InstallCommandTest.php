3e3865a47abea9dfc36b11d91de00c4ed1d4c007
agoalofalife <agoalofalife@gmail.com>
2017-10-12 15:03:56 +0300

    test console

---
+++ tests/Unit/Console/InstallCommandTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chubarov
+ * Date: 12.10.17
+ * Time: 14:19
+ */
+
+class InstallCommandTest
+{
+
+}
---
+++ tests/Unit/Console/ParseCommandTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chubarov
+ * Date: 12.10.17
+ * Time: 14:59
+ */
+
+namespace agoalofalife\Tests\Console;
+
+
+class ParseCommandTest
+{
+
+}
