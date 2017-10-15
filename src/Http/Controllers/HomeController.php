b68c1d3ccdfc2b912475bf523ab0a36f6be42b6d
agoalofalife <agoalofalife@gmail.com>
2017-10-15 14:36:45 +0300

    set middleware

--- src/Http/Controllers/HomeController.php
+++ src/Http/Controllers/HomeController.php
@@ -5,6 +5,13 @@ use Illuminate\View\View;
 
 class HomeController
 {
+    public function __construct()
+    {
+        if (class_exists(config('postman.middleware'))) {
+            $this->middleware(config('postman.middleware'));
+        }
+    }
+
     public function index() : View
     {
         return view('postman::app');
