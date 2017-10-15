f7397d8b4e6dd0b84ce2487eed04623a5189595c
agoalofalife <agoalofalife@gmail.com>
2017-10-15 11:37:51 +0300

    refactoring

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,7 +2,6 @@ import Vue from 'vue/dist/vue.js';
 import axios from 'axios'
 import App from './components/App.vue';
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
 import lang from 'element-ui/lib/locale/lang/en'
 import locale from 'element-ui/lib/locale'
 import VueHtml5Editor from 'vue-html5-editor'
@@ -42,51 +41,6 @@ Vue.use(VueHtml5Editor, {
     },
     //default en-us, en-us and zh-cn are built-in
     language: "en-us",
-    i18n: {
-        //specify your language here
-        // "zh-cn": {
-        //     "align": "对齐方式",
-        //     "image": "图片",
-        //     "list": "列表",
-        //     "link": "链接",
-        //     "unlink": "去除链接",
-        //     "table": "表格",
-        //     "font": "文字",
-        //     "full screen": "全屏",
-        //     "text": "排版",
-        //     "eraser": "格式清除",
-        //     "info": "关于",
-        //     "color": "颜色",
-        //     "please enter a url": "请输入地址",
-        //     "create link": "创建链接",
-        //     "bold": "加粗",
-        //     "italic": "倾斜",
-        //     "underline": "下划线",
-        //     "strike through": "删除线",
-        //     "subscript": "上标",
-        //     "superscript": "下标",
-        //     "heading": "标题",
-        //     "font name": "字体",
-        //     "font size": "文字大小",
-        //     "left justify": "左对齐",
-        //     "center justify": "居中",
-        //     "right justify": "右对齐",
-        //     "ordered list": "有序列表",
-        //     "unordered list": "无序列表",
-        //     "fore color": "前景色",
-        //     "background color": "背景色",
-        //     "row count": "行数",
-        //     "column count": "列数",
-        //     "save": "确定",
-        //     "upload": "上传",
-        //     "progress": "进度",
-        //     "unknown": "未知",
-        //     "please wait": "请稍等",
-        //     "error": "错误",
-        //     "abort": "中断",
-        //     "reset": "重置"
-        // }
-    },
     // the modules you don't want
     hiddenModules: [
         "full-screen",
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,6 @@
 <div>
     <div id="root"></div>
-    <div style="height: 0; width: 0; position: absolute; display: none;">
-        {{--    {!! file_get_contents(public_path('/vendor/postman/img/sprite.svg')) !!}--}}
-    </div>
     <script src="{{ mix('js/app.js', 'vendor/postman') }}"></script>
 </div>
-
+<link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-default/index.css">
 <link rel="stylesheet" href="https://opensource.keycdn.com/fontawesome/4.7.0/font-awesome.min.css" integrity="sha384-dNpIIXE8U05kAbPhy3G1cz+yZmTzA6CY8Vg/u2L9xRnHjJiAK76m2BIEaSEV+/aU" crossorigin="anonymous">
