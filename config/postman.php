5c7499d7942adbfd5cb5aeb427d4773344cff041
agoalofalife <agoalofalife@gmail.com>
2017-10-15 11:47:37 +0300

    add comment

--- config/postman.php
+++ config/postman.php
@@ -1,10 +1,23 @@
 <?php
 return [
     'switcher' => true,
+     /*
+     |--------------------------------------------------------------------------
+     | List modes classes
+     |--------------------------------------------------------------------------
+     | All list classes mode. Here you can add custom modes.
+     */
     'modes' => [
         \agoalofalife\postman\Modes\OneToAll::class,
         \agoalofalife\postman\Modes\Each::class,
     ],
+
+      /*
+      |--------------------------------------------------------------------------
+      | Sizes table
+      |--------------------------------------------------------------------------
+      | You can set your sizes for table
+      */
     'ui' => [
         'table' => [
             'id' => 60,
